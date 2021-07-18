bb8ca2b32bf30a9d939d4c9dacdf219e8e7dcfb7
Pavornoc <pavornoc@protonmail.ch>
2021-07-18 12:37:06 -0400

    Initial commit to GitHub of PythonHunt project.

---
+++ README.md
@@ -0,0 +1,209 @@
+# Python Hunt
+## Quick OSINT checks for IPs and Domains during triage and investigations.
+
+---
+
+### About
+This script queries APIs for various freely-available intelligence platforms
+in order to gain important context and reputation data for IP addresses and/or
+domains.  
+
+---
+
+#### Platforms Used
+* WHOIS
+* VirusTotal
+* AlienVault OTX
+* Robtex
+* IBM X-Force
+* Shodan
+* ipinfo.io
+
+
+##### API Keys Required for:
+* VirusTotal
+* IBM X-Force
+* Shodan
+
+If you do not wish to create an account to get an API key for these platforms,
+you can comment out their functions at the end of the code and just use the
+data from WHOIS, AlienVault, Robtex, and IP Info.
+
+
+#### Installation and Requirements
+This is built for Python 3.x.
+
+Install required modules with:
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+You will need to also edit the main script to include your API keys for a few
+of the platforms.  Do this in the "API Key" section near the top of the code.
+
+If you'd like to link this in your CLI $PATH, perform the following:
+```bash
+$ ln -s /path/to/repo/investigate.py /usr/local/bin/investigate
+```
+Reload your terminal (new window, log off and back on, etc) and `investigate`
+should be a native command.  You can now call it by executing directly.
+```bash
+$ investigate
+```
+
+
+#### Example Usage
+Python Hunt can take single IPs as command line argument with -i or --ipaddress
+```bash
+$ investigate -i 95.217.163.246
+```
+
+It can also perform a lookup for domains with -d or --domain flags
+```bash
+$ investigate -d apple.com
+```
+Finally, it can check a file for a list of IPs or Domains.
+You may mix types in the file, but they must be 1 per line.
+You can do this by using -f or --file
+
+```bash
+$ investigate -f IoC_file.txt
+```
+Unfortunately, due to API rate limiting with the free APIs, you may only
+look up 5 items per minute.  If you have paid for API keys without limits,
+you may adjust the code to omit this rate limit check.
+
+
+#### Example Output
+
+```bash
+$ investigate -i 193.34.167.111
+_________________________________________
+
+    Investigating 193.34.167.111:
+
+    Connecting from Utrecht, Utrecht; NL.
+    IP belongs to AS62370 Snel.com B.V..
+
+
+    Shodan
+    ----------
+
+    Geolocation double-check:
+        Utrecht, Netherlands, 09; Owned by Snel.com B.V..
+
+
+    Additional Shodan Info:
+        OS: No Data
+        Port(s): [80, 123, 443]
+        Hostname: ['srv-1.canajambe.io']
+        Last Updated: 2021-03-22T09:12:54.583341
+
+
+    VirusTotal
+    ----------
+
+    Scan Stats:
+    Country is not in VT dataset.
+    AS Owner is not found.
+    Harmless: No Data
+    Suspicious: No Data
+    Malicious: No Data
+    Undetected: No Data
+
+
+    AlienVault OTX
+    ----------
+
+    Pulse Count: 3
+    Reputation Score: 0
+    Pulse Name(s): New macOS malware XcodeSpy Targets Xcode Developers with EggShell Backdoor, New macOS malware XcodeSpy Targets Xcode Developers with EggShell Backdoor, New macOS malware XcodeSpy Targets Xcode Developers with EggShell Backdoor - SentinelLabs
+
+
+
+    IBM X-Force
+    ----------
+
+    Recent Report Created: 2012-03-22T07:26:00.000Z
+    IP Geolocation: Netherlands
+    Reputation Score: 1
+    Report Created Reason: One of the five RIRs announced a (new) location mapping of the IP.
+
+
+    Robtex
+    ----------
+
+    County: Netherlands
+    ASN: 62370, Snel
+    WHOIS Desc.: To determine the registration information for a more specific range, please try a more specific query. If you see this object as a result of a single IP query, it means the IP address is currently in the free pool of address space managed by the RIPE NCC.
+    BGP Route: 193.34.166.0/23
+    Active DNS Record: hosted-by.snelis.com
+    Active DNS History: None
+    Passive DNS: None
+    Passive DNS History: ns2.hartelust.com, ns2.pernillasart.com, ns2.vesconet.com
+
+  ```
+
+---
+
+```bash
+$ investigate -d creditkarma.com
+__________________________________________________
+
+    Investigating Domain "creditkarma.com"
+
+
+    WHOIS
+    ----------
+
+    Created on 2005-08-30 02:56:23
+    Expires on 2021-08-30 02:56:23
+    Registrar: CSC Corporate Domains, Inc.
+    Last Updated: 2019-04-15 21:04:38
+    Registered in: US
+    Name Servers: ns1.p16.dynect.net, ns2.p16.dynect.net, ns3.p16.dynect.net, ns4.p16.dynect.net
+
+
+    VirusTotal
+    ----------
+
+    Domain Created: 2005-08-29 10:56:23
+    Alexa Rank: 355
+    Cisco Umbrella Rank: 24496
+    Overall Reputation: 0
+    Harmless: 75
+    Suspicious: None.
+    Malicious: None.
+    Undetected: 7
+
+
+    AlienVault OTX
+    ----------
+
+    Pulse Count: 1
+    Reputation Score: No Data
+    Pulse Name(s): xdgcdn.com -  cdn-me.com - squatting - phishing - icloud - itunes - music - inaudible - bluetooth - nfc - hacking the human
+
+
+    IBM X-Force
+    ----------
+
+    Name: Credit Karma
+    Score: 1
+    Description: A free credit and financial management platform
+    Categories: Financial Services / Insurance / Real Estate, Banking
+
+```
+
+
+---
+
+#### Thank you to the following projects
+* [Python Whois](https://pypi.org/project/python-whois/)
+* [VirusTotal](https://virustotal.com)
+* [AlienVault](https://otx.alienvault.com)
+* [IP Info](https://ipinfo.io)
+* [Shodan](https://shodan.io)
+* [IBM X-Force Exchange](https://exchange.xforce.ibmcloud.com/)
+* [Robtex](https://www.robtex.com)
+
---
+++ investigate.py
@@ -0,0 +1,567 @@
+#!/usr/bin/env python3
+
+"""
+Script to assist in investigations by collecting IP data from various sources.
+"""
+
+import argparse
+import sys
+from datetime import datetime
+
+import requests
+import shodan
+import whois
+
+# API Keys
+# API Keys are required for Shodan, VirusTotal, and IBM X-Force
+# Register with them here:
+# https://shodan.io
+# https://virustotal.com
+# https://exchange.xforce.ibmcloud.com/
+
+SHODAN_API = "[YOUR API KEY HERE]"
+VT_API = "[YOUR API KEY HERE]"
+# X-Force API is not actually the API key, but the base64 value of the API Key
+# and the API Password.  Do a test at the following documentation site to get
+# the value from the cURL request:
+# https://api.xforce.ibmcloud.com/doc/#IP_Reputation_get_ipr_ip
+XFORCE_API = "[YOUR API KEY HERE]"
+
+
+def main():
+    """
+    Defining main parser for arguments passed to the script.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Investigate an IP address or Domain for available OSINT."
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-i", "--ipaddress", help="IP to investigate.")
+    group.add_argument("-d", "--domain", help="Domain to investigate.")
+    group.add_argument(
+        "-f",
+        "--file",
+        help="File containing a list of IPs (1 per line, up to 5 due to ratelimiting).",
+    )
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    if args.ipaddress:
+        ip_check(args.ipaddress)
+    elif args.domain:
+        domain_check(args.domain)
+    elif args.file:
+        targets_processed_count = 0
+        with open(args.file) as file:
+            for target in file:
+                if targets_processed_count > 5:
+                    print("VirusTotal has a rate limit of 5 checks, stopping.")
+                    break
+                clean = target.strip()
+                type = clean.replace(".", "").replace(":", "").replace("/", "")
+                if type.isdigit():
+                    targets_processed_count += 1
+                    ip_check(clean)
+                elif type.isalpha():
+                    targets_processed_count += 1
+                    domain_check(clean)
+                else:
+                    print(f"Skipping {clean}, can't determine the type.")
+
+# Start of IP Check functions
+
+
+def geo_info(target):
+    """
+    Basic geolocation and IP ownership information.
+    """
+    data = requests.get(f"https://ipinfo.io/{target}/json").json()
+    print(
+        """_________________________________________
+
+    Investigating {}:
+
+    Connecting from {}, {}; {}.
+    IP belongs to {}.
+        """.format(
+            data.get("ip", "IP Not Found."),
+            data.get("city", "Not Found."),
+            data.get("region", "Not Found."),
+            data.get("country", "Not Found."),
+            data.get("org", "Not Found."),
+        )
+    )
+
+
+def shodan_check(target):
+    """
+    Double check geo and org information with Shodan, as well as pull
+    additional information on the host.
+    """
+    print(
+        """
+    Shodan
+    ----------"""
+    )
+    try:
+        data = shodan.Shodan(SHODAN_API).host(target)
+    except shodan.APIError as error:
+        print(error)
+    else:
+        print(
+            """
+    Geolocation double-check:
+        {}, {}, {}
+        Owned by {}.""".format(
+                data.get("city", "No Data."),
+                data.get("country_name", "No Data"),
+                data.get("region_code", "No Data."),
+                data.get("org", "No Data."),
+            )
+        )
+        print(
+            """
+    Additional Shodan Info:
+        OS: {}
+        Port(s): {}
+        Hostname: {}
+        Last Updated: {}
+            """.format(
+                data.get("os", "No Data."),
+                data.get("ports", "No Data."),
+                data.get("hostnames", "No Data."),
+                data.get("last_update", "No Data"),
+            )
+        )
+
+
+def vt_ip_check(target):
+    """
+    Checks VirusTotal for known malicious actions/malware associated with the IP.
+    """
+    url = f"https://www.virustotal.com/api/v3/ip_addresses/{target}"
+    headers = {"x-apikey": VT_API}
+    response = requests.get(url, headers=headers)
+    attributes = response.json().get("data", {}).get("attributes", {})
+    last_analysis = attributes.get("last_analysis_stats", {})
+    print(
+        """
+    VirusTotal
+    ----------"""
+    )
+    if response.status_code == 200:
+        print(
+            """
+    Scan Stats:
+    Country is {}
+    AS Owner is {}
+    Harmless: {}
+    Suspicious: {}
+    Malicious: {}
+    Undetected: {}
+            """.format(
+                attributes.get("country", "Not Found"),
+                attributes.get("as_owner", "Not Found"),
+                last_analysis.get("harmless", "None."),
+                last_analysis.get("suspicious", "None."),
+                last_analysis.get("malicious", "None."),
+                last_analysis.get("undetected", "None."),
+            )
+        )
+    else:
+        print(
+            """
+    Error:
+    VirusTotal Response Code {}""".format(
+                response.status_code
+            )
+        )
+
+
+def av_otx(target):
+    """
+    Checks IP to see if it shows up in any "pulse", which is a crowdsourced
+    datafeed for malware/malicious action.
+    """
+    url = f"https://otx.alienvault.com/api/v1/indicators/IPv4/{target}"
+    response = requests.get(url)
+    data = response.json()
+
+    print(
+        """
+    AlienVault OTX
+    ----------"""
+    )
+    if response.status_code == 200:
+        pulse_count = data.get("pulse_info", {}).get("count")
+        if pulse_count > 0:
+            pulse_name = [item["name"] for item in data["pulse_info"]["pulses"]]
+
+            print(
+                """
+    Pulse Count: {}
+    Reputation Score: {}
+    Pulse Name(s): {}
+                """.format(
+                    pulse_count,
+                    data.get("reputation", "No Data"),
+                    ", ".join(pulse_name) if pulse_name else "No Data",
+                )
+            )
+
+        elif pulse_count == 0:
+            print(
+                """
+    No findings for this IP.
+                """
+            )
+    else:
+        print(
+            """
+    Error: OTX response code: {}""".format(
+                response.status_code
+            )
+        )
+
+
+def robtex(target):
+    """
+    Robtex provides acitve and passive DNS data, as well as BGP routing data.
+    """
+    # Robtex doesn't accept the Python UA for some reason, so we need to set a
+    # custom UA.  This one is mimicking Firefox 86 on MacOS Big Sur.
+    # Change if desired.
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:86.0) Gecko/20100101 Firefox/86.0"
+    }
+    data = requests.get(
+        f"https://freeapi.robtex.com/ipquery/{target}", headers=headers
+    ).json()
+    status = data["status"]
+    print(
+        """
+    Robtex
+    ----------"""
+    )
+    if status == "ok":
+        active_dns = [hit["o"] for hit in data.get("act", [])]
+        active_hist = [hit["o"] for hit in data.get("acth", [])]
+        passive_dns = [hit["o"] for hit in data.get("pas", [])]
+        passive_hist = [hit["o"] for hit in data.get("pash", [])]
+        print(
+            """
+    County: {}
+    ASN: {}, {}
+    WHOIS Desc.: {}
+    BGP Route: {}
+    Active DNS Record: {}
+    Active DNS History: {}
+    Passive DNS: {}
+    Passive DNS History: {}
+                """.format(
+                data.get("country", "No Data."),
+                data.get("as", "No Data."),
+                data.get("asname", "No Data."),
+                data.get("whoisdesc", "No Data."),
+                data.get("bgproute", "No Data."),
+                ", ".join(active_dns) if active_dns else "None",
+                ", ".join(active_hist) if active_hist else "None",
+                ", ".join(passive_dns) if passive_dns else "None",
+                ", ".join(passive_hist) if passive_hist else "None",
+            )
+        )
+    elif status == "ratelimited":
+        print(
+            """
+    API Rate Limit reached. Try again soon.
+        """
+        )
+    else:
+        print(
+            """
+    Robtex has no records.
+        """
+        )
+
+
+def xforce_ip(target):
+    """
+    Checking IBM's X-Force Echange for crowdsourced intelligence on the IP.
+    """
+    url = f"https://api.xforce.ibmcloud.com/ipr/{target}"
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic {}".format(XFORCE_API),
+    }
+    response = requests.get(url=url, headers=headers)
+    print(
+        """
+    IBM X-Force
+    ----------"""
+    )
+    if response.status_code == 200:
+        data = response.json()
+        print(
+            """
+    Recent Report Created: {}
+    IP Geolocation: {}
+    Reputation Score: {}
+    Report Created Reason: {}
+            """.format(
+                data.get("history", {})[0].get("created", "No Data."),
+                data.get("history", {})[0].get("country", "No Data."),
+                data.get("history", {})[0].get("score", "No Data"),
+                data.get("history", {})[0].get("reasonDescription", "No Data."),
+            )
+        )
+    else:
+        print(
+            """
+    X-Force Response Code: {}
+            """.format(
+                response.status_code
+            )
+        )
+
+
+# Start of Domain Check functions
+
+
+def whois_lookup(target):
+    """
+    Basic WHOIS data lookup. Uses https://github.com/DannyCork/python-whois/
+    """
+    try:
+        domain = whois.query(target)
+        if domain is None:
+            print("Domain not found in WHOIS")
+            name = "Not Found."
+            created = "No Data."
+            expires = "No Data."
+            updated = "No Data."
+            reg = "No Data."
+            country = "No Data."
+            nameservers = None
+            ns_clean = None
+        else:
+            name = domain.name
+            created = domain.creation_date
+            expires = domain.expiration_date
+            updated = domain.last_updated
+            reg = domain.registrar
+            country = domain.registrant_country
+            nameservers = domain.name_servers
+            ns_clean = set(server.strip() for server in nameservers)
+    except AttributeError as error:
+        print(error)
+    else:
+        print(
+            """__________________________________________________
+
+    Investigating Domain "{}"
+
+
+    WHOIS
+    ----------
+
+    Created on {}
+    Expires on {}
+    Registrar: {}
+    Last Updated: {}
+    Registered in: {}
+    Name Servers: {}
+        """.format(
+                name if name else "No Data",
+                created if created else "No Data",
+                expires if expires else "No Data",
+                reg if reg else "No Data",
+                updated if updated else "No Data",
+                country if country else "No Data",
+                ", ".join(sorted(list(ns_clean))) if ns_clean else "No Data",
+            )
+        )
+
+
+def vt_domain_check(target):
+    """
+    Checks VirusTotal for known malicious actions/malware associated with the Domain.
+    """
+    url = f"https://www.virustotal.com/api/v3/domains/{target}"
+    headers = {"x-apikey": VT_API}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    attributes = data.get("data", {}).get("attributes", {})
+    rank = data.get("data", {}).get("attributes", {}).get("popularity_ranks", {})
+    last_analysis = (
+        data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+    )
+    creation_date = attributes.get("creation_date")
+    print(
+        """
+    VirusTotal
+    ----------"""
+    )
+    if response.status_code == 200:
+        print(
+            """
+    Domain Created: {}
+    Alexa Rank: {}
+    Cisco Umbrella Rank: {}
+    Overall Reputation: {}
+    Harmless: {}
+    Suspicious: {}
+    Malicious: {}
+    Undetected: {}
+            """.format(
+                datetime.fromtimestamp(creation_date).strftime("%Y-%m-%d %I:%M:%S")
+                if creation_date
+                else "None",
+                rank.get("Alexa", {}).get("rank", "No Data."),
+                rank.get("Cisco Umbrella", {}).get("rank", "No Data."),
+                attributes.get("reputation", "No Data."),
+                last_analysis.get("harmless", "None."),
+                last_analysis.get("suspicious", "None."),
+                last_analysis.get("malicious", "None."),
+                last_analysis.get("undetected", "None."),
+            )
+        )
+    else:
+        print(
+            """
+    Error:
+    VirusTotal Response Code {}""".format(
+                response.status_code
+            )
+        )
+
+
+def av_otx_domain(target):
+    """
+    Checks Domain to see if it shows up in any "pulse", which is a crowdsourced
+    datafeed for malware/malicious action.
+    """
+    url = f"https://otx.alienvault.com/api/v1/indicators/domain/{target}"
+    response = requests.get(url)
+    data = response.json()
+    pulse_count = data.get("pulse_info", {}).get("count", "No Data")
+    print(
+        """
+    AlienVault OTX
+    ----------"""
+    )
+    if response.status_code == 200:
+        if pulse_count > 0:
+            try:
+                pulse_name = [item["name"] for item in data["pulse_info"]["pulses"]]
+            except TypeError as error:
+                print(error)
+            print(
+                """
+    Pulse Count: {}
+    Reputation Score: {}
+    Pulse Name(s): {}
+                    """.format(
+                    pulse_count,
+                    data.get("reputation", "No Data."),
+                    ", ".join(pulse_name) if pulse_name else "No Data",
+                )
+            )
+        elif pulse_count == 0:
+            print(
+                """
+    No findings for this domain.
+                    """
+            )
+    else:
+        print(
+            """
+    Error: OTX Response Code {}
+            """.format(
+                response.status_code
+            )
+        )
+
+
+def xforce_domain(target):
+    """
+    Checking IBM's X-Force Echange for crowdsourced intelligence on the domain.
+    """
+    url = "https://api.xforce.ibmcloud.com/url/{}".format(target)
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic {}".format(XFORCE_API),
+    }
+    response = requests.get(url=url, headers=headers)
+    data = response.json()
+    print(
+        """
+    IBM X-Force
+    ----------"""
+    )
+
+    if response.status_code == 200:
+        try:
+            categories = data["result"]["cats"].keys()
+        except TypeError as error:
+            print(error)
+        else:
+            application = data.get("result", {}).get("application", {})
+            print(
+                """
+    Name: {}
+    Score: {}
+    Description: {}
+    Categories: {}
+                """.format(
+                    application.get("name", "Not Found."),
+                    data.get("result", {}).get("score", "Not Found"),
+                    application.get("description", "Not Found."),
+                    ", ".join(categories),
+                )
+            )
+    elif response.status_code == 404:
+        print(
+            """
+    Domain not in X-Force database
+            """
+        )
+    else:
+        print(
+            """
+    Error: X-Force Response Code: {}
+            """.format(
+                response.status_code
+            )
+        )
+
+
+def ip_check(target):
+    """
+    Collection of all IP check functions to run.
+    Comment out specific ones if you do not have an API key, as necessary.
+    """
+    geo_info(target)
+    shodan_check(target)
+    vt_ip_check(target)
+    av_otx(target)
+    xforce_ip(target)
+    robtex(target)
+
+
+def domain_check(target):
+    """
+    Collection of all Domain check functions to run.
+    Comment out specific ones if you do not have an API key, as necessary.
+    """
+    whois_lookup(target)
+    vt_domain_check(target)
+    av_otx_domain(target)
+    xforce_domain(target)
+
+
+if __name__ == "__main__":
+    main()
